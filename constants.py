8728ee1dd381d581c21d781f073f6b261bc89806
mauwazahmed <56031816+mauwazahmed@users.noreply.github.com>
2024-01-15 22:02:16 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 import os 
 import chromadb
 from pydantic_settings import BaseSettings,SettingsConfigDict
-CHROMA_SETTINGS = SettingsConfigDict(
+
+class Settings(BaseSettings):
+    CHROMA_SETTINGS = SettingsConfigDict(
         chroma_db_impl='duckdb+parquet',
         persist_directory='db',
         anonymized_telemetry=False
-)
+    )
